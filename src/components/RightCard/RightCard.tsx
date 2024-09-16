52cc0655661c68a563b2e58b2e63e894b9711ff5
bang-jeeewon <giw0233@gmail.com>
2024-09-16 12:52:16 +0900

    Feat: update

--- src/components/RightCard/RightCard.tsx
+++ src/components/RightCard/RightCard.tsx
@@ -1,33 +1,33 @@
-'use client'
+'use client';
 
-import { Card, Divider, Row, Col } from 'antd'
-import './rightCard.css'
-import sbLogoImg from '/softberry.svg'
-import Project from '../Project/Project'
-import { useEffect, useState } from 'react'
-import animation from '../../styles/animations.module.css'
+import { Card, Divider, Row, Col } from 'antd';
+import './rightCard.css';
+import sbLogoImg from '/softberry.svg';
+import Project from '../Project/Project';
+import { useEffect, useState } from 'react';
+import animation from '../../styles/animations.module.css';
 
 const RightCard = () => {
-  const [period, setPeriod] = useState('')
+  const [period, setPeriod] = useState('');
 
   const computedPeriod = () => {
-    const startDate = new Date('2022-03-08')
-    const currentDate = new Date()
+    const startDate = new Date('2022-03-08');
+    const currentDate = new Date();
 
-    let years = currentDate.getFullYear() - startDate.getFullYear()
-    let months = currentDate.getMonth() - startDate.getMonth()
+    let years = currentDate.getFullYear() - startDate.getFullYear();
+    let months = currentDate.getMonth() - startDate.getMonth();
 
     if (months < 0) {
-      years--
-      months += 12
+      years--;
+      months += 12;
     }
 
-    setPeriod(`${years}년 ${months}개월`)
-  }
+    setPeriod(`${years}년 ${months}개월`);
+  };
 
   useEffect(() => {
-    computedPeriod()
-  }, [])
+    computedPeriod();
+  }, []);
 
   return (
     <>
@@ -50,7 +50,6 @@ const RightCard = () => {
               </Col>
             </Row>
 
-            {/* <Suspense fallback={<Skeleton active />}> */}
             <Project
               title='스마트시스템 (가제)'
               stacks={['Next.js', 'TypeScript', 'TailwindCSS', 'Zustand', 'AWS API Gateway', 'Vercel']}
@@ -69,32 +68,9 @@ const RightCard = () => {
                 '프로젝트 생성 및 초기화, 코드 구조 셋업',
                 '모바일과 키오스크 해상도에 최적화된 반응형 디자인 구현',
                 'AWS API Gateway Mock API 구현',
-                'AWS S3, Route 53, CloudFront를 활용한 프론트엔드 서버 호스팅',
+                'Vercel 활용하여 CI/CD 구축',
               ]}
               imgs={[]}
-            />
-            {/* </Suspense> */}
-
-            <Project
-              title='EVI Hub Client Admin 웹 서비스 개발'
-              stacks={['Vue3', 'TypeScript', 'Vuetify3', 'Vite', 'Pinia', 'AWS CodePipeline']}
-              period='2022.03 ~ 현재'
-              front={1}
-              back={2}
-              url=''
-              descriptions={[
-                'Vue3의 빌트인 컴포넌트 Suspense 사용하여 비동기로 렌더링',
-                'AWS CodePipeline과 CodeBuild를 활용하여 CI/CD 파이프라인 구축',
-                '함수 기반 유연한 코드 구조를 적용하기 위해 Composition API 사용',
-                'Google Maps API 활용하여 지도 기능 구현 및 커스터마이징',
-                '시간표 관련 라이브러리가 없어 직접 맞춤형 솔루션을 설계하고 구현',
-                'i18n 라이브러리를 사용하여 다국어 처리 구현',
-                'AWS EC2 Java에 Vue를 설치 및 빌드하여 배포되어있던 구조를 Vue는 단독으로 S3로 분리하고 CloudFront CDN 적용하여 로딩속도 개선',
-                '기존의 조회 기능이 POST 메서드로 구현된 문제를 해결하기 위해 백엔드 개발자와 협력하여 RESTful API에 맞춰 새로운 Client 전용 서비스로 재구현',
-                '코드 가독성과 유지보수성을 위해 TypeScript를, 성능 개선과 Composition API와 같은 신기능 사용을 위해 Vue 3로, 빌드 속도 개선과 빠른 개발 서버 시작을 위해 Webpack을 Vite로 마이그레이션',
-                '마이그레이션 후 S3 버킷 크기는 32%, 객체 수는 49%로 축소되어 성능 개선 경험',
-              ]}
-              imgs={['/ca-home.png', '/ca-price.png']}
             />
 
             <Project
@@ -119,7 +95,29 @@ const RightCard = () => {
             />
 
             <Project
-              title='NH올원뱅크 웹뷰 개발'
+              title='EVI Hub Client Admin 웹 서비스 개발'
+              stacks={['Vue3', 'TypeScript', 'Vuetify3', 'Vite', 'Pinia', 'AWS CodePipeline']}
+              period='2022.03 ~ 현재'
+              front={1}
+              back={2}
+              url=''
+              descriptions={[
+                'Vue3의 빌트인 컴포넌트 Suspense 사용하여 비동기로 렌더링',
+                'AWS CodePipeline과 CodeBuild를 활용하여 CI/CD 파이프라인 구축',
+                '함수 기반 유연한 코드 구조를 적용하기 위해 Composition API 사용',
+                'Google Maps API 활용하여 지도 기능 구현 및 커스터마이징',
+                '시간표 관련 라이브러리가 없어 직접 맞춤형 솔루션을 설계하고 구현',
+                'i18n 라이브러리를 사용하여 다국어 처리 구현',
+                'AWS EC2 Java에 Vue를 설치 및 빌드하여 배포되어있던 구조를 Vue는 단독으로 S3로 분리하고 CloudFront CDN 적용하여 로딩속도 개선',
+                '기존의 조회 기능이 POST 메서드로 구현된 문제를 해결하기 위해 백엔드 개발자와 협력하여 RESTful API에 맞춰 새로운 Client 전용 서비스로 재구현',
+                '코드 가독성과 유지보수성을 위해 TypeScript를, 성능 개선과 Composition API와 같은 신기능 사용을 위해 Vue 3로, 빌드 속도 개선과 빠른 개발 서버 시작을 위해 Webpack을 Vite로 마이그레이션',
+                '마이그레이션 후 S3 버킷 크기는 32%, 객체 수는 49%로 축소되어 성능 개선 경험',
+              ]}
+              imgs={['/ca-home.png', '/ca-price.png']}
+            />
+
+            <Project
+              title='NH올원뱅크 & NHpay 앱 웹뷰 개발'
               stacks={['Nuxt.js', 'JavaScript', 'TailwindCSS', 'Google Analytics']}
               period='2023.07 ~ 2023.09'
               front={1}
@@ -137,7 +135,7 @@ const RightCard = () => {
         </Card>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default RightCard
+export default RightCard;
